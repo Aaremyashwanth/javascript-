3256bfd53bb13be4871ac46ea90c5de7656db11c
aaremyashwanth <111639396+Aaremyashwanth@users.noreply.github.com>
2023-11-25 16:03:02 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+let fruitlist=["apple","bananna","guava","mango"];
+console.log(typeof fruitlist);
+let bestfruit=fruitlist[2];
+console.log(bestfruit);
+let cake=0;
+console.log(typeof cake);
+fruitlist[0]="man";
+console.log(fruitlist);
